071548551b157b47cec6a6c4a511ca8c57225885
Nikolay Alexeyev <nalexeyev@users.noreply.github.com>
2018-09-11 15:45:24 +0300

    Interim non-working version

--- SWCert-APS1-TaskB/SWCert-APS1-TaskB/SWCert-APS1-TaskB.cpp
+++ SWCert-APS1-TaskB/SWCert-APS1-TaskB/SWCert-APS1-TaskB.cpp
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
-#define ARR_MAX 200
+#define ARR_MAX 6
 
 #define DEBUG
 
@@ -12,17 +12,83 @@
 #define freopen_dbg(...)
 #endif
 
+int prize_arr[ARR_MAX] = { -1, -1, -1, -1, -1, -1 };
+int reshuffles = 0;
+int length = -1;
+int prize = 0;
+int i = 0, j = 0, ii = 0;
+
+
+int intarr2int(int *arr, int len_arr)
+{
+	int i = 0;
+	int result = 0;
+
+	for (i = 0; i < len_arr; i++) {
+		result *= 10;
+		result += arr[i];
+	}
+	return result;
+}
 
 int main()
 {
-	long prize;
-	int reshuffles;
-
 	freopen_dbg("inputfile.txt", "r", stdin);
+	
+	// read number as digits array
+	for (i = 0; i < ARR_MAX; i++) {
+		int c = getc(stdin);
+		if ((c == EOF) || (c == ' '))
+			break;
+		prize_arr[i] = c - '0'; // why?
+	}
+	length = i;
 
-	scanf("%d %d\n", &prize, &reshuffles);
+	scanf("%d\n", &reshuffles);
 
-	printf_dbg("%d %d\n", prize, reshuffles);
+	int max_item, min_item;
+	int max_index, min_index;
+	int tmp;
+
+	printf_dbg("%d\n", intarr2int(prize_arr, length));
+
+	for (i = 0; i < reshuffles; i++) {
+		printf_dbg("Shuffle #%d\n", i);
+		max_item = -1;
+		min_item = 10;
+		for (j = length - 1 - i; j >= (0 + i); j--) { // the max of similar max'es is right one
+			if (prize_arr[j] > max_item) {
+				max_item = prize_arr[j];
+				max_index = j;
+			}
+			if (prize_arr[j] < min_item) {
+				min_item = prize_arr[j];
+				min_index = j;
+			}
+		}
+
+		printf_dbg("max: %d(%d) min:%d(%d)\n", max_item, max_index+1, min_item, min_index+1);
+
+		// move max to first
+		if (1) {
+			tmp = prize_arr[i];
+			prize_arr[i] = prize_arr[max_index];
+			prize_arr[max_index] = tmp;
+		}
+
+		printf_dbg("%d\n", intarr2int(prize_arr, length));
+
+		// reshuffle min to last
+		if (1) {
+			tmp = prize_arr[length-1-i];
+			prize_arr[length - 1 - i] = prize_arr[min_index];
+			prize_arr[min_index] = tmp;
+		}
+
+		printf_dbg("%d\n", intarr2int(prize_arr, length));
+	}
+	
+	printf_dbg("Answer: %d\n", printf_dbg("%d\n", intarr2int(prize_arr, length)));
 
     return 0;
 }
